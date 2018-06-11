9de7a639cc5e5f73d9a9613793e78402f3d30392
Shivanand C Kohalli <shivanandk58@gmail.com>
2018-06-11 02:27:00 +0200

    Combining weather data and LORA parameters

--- scrape_weather/scrape_weather.py
+++ scrape_weather/scrape_weather.py
@@ -6,18 +6,19 @@ import os
 directory = "./dump"  # To store the scraped data
 
 api_key = "" # Add your api key here
-latitude = "52.379189"
-longitude = "4.899431"
+latitude = "52.022590"
+longitude = "4.692522"
 
 if not os.path.exists(directory):
 	os.makedirs(directory)
-date = datetime.datetime(2016,1,1)
-for i in range(1000): 
+date = datetime.datetime(2017,02,1)
+for i in range(450): 
     date += datetime.timedelta(days=1)
     epoch = calendar.timegm(date.timetuple())
     file = open(directory + "/" + str(epoch), "w")
     req = requests.get("https://api.darksky.net/forecast/" + api_key + "/"+ latitude+"," + longitude +"," + str(epoch) + "?exclude=currently,minutely,daily,alerts")
     file.write(req.content)
     file.close()
+    print i
 
 print ("Completed")
